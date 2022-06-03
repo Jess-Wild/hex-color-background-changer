a6ee741fdddda0ec2f4c48da0a7ffb90707fc915
Jess-Wild <j.victorion.wcs@gmail.com>
2022-06-03 12:40:19 +0200

    Finished

--- js/app.js
+++ js/app.js
@@ -1,19 +1,17 @@
-(function() {
-    const button = document.querySelector('#btn')
-    const body = document.querySelector('body')
-    const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
-    const value = document.querySelector('#hex-value')
+const button = document.querySelector("#btn");
+const body = document.querySelector("body");
+const hexValue = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const valueColor = document.querySelector("#hex-value");
 
-    button.addEventListener('click', changeHex)
+button.addEventListener("click", function(){
+    // function changeColor(){
+        let hexColor = "#";
 
-    function changeHex(){
-        let hex = '#'
-
-        for (let i = 0; i < 6; i++){
-            const index = Math.floor(Math.random()*hexValues.length)
-            hex += hexValues[index]
+        for(let i = 0; i < 6; i++){
+            const value = Math.floor(Math.random() * hexValue.length);
+            hexColor += hexValue[value];
         }
-        value.textContent = hex
-        body.style.backgroundColor = hex
-    }
-} )()
+        valueColor.textContent = hexColor;
+        body.style.backgroundColor = hexColor;
+    // }
+});
